25c51dac724d26a36e7a407dff5feba9741bf98e
naifmhd <naifmhd@gmail.com>
2015-03-22 00:28:01 +0500

    After Signed-off-by: naifmhd <naifmhd@gmail.com>

---
+++ Material/app/src/main/java/com/knaive/naif/material/Information.java
@@ -0,0 +1,9 @@
+package com.knaive.naif.material;
+
+/**
+ * Created by Naif on 3/21/2015.
+ */
+public class Information {
+    int iconId;
+    String title;
+}
---
+++ Material/app/src/main/java/com/knaive/naif/material/ViewAdapter.java
@@ -0,0 +1,66 @@
+package com.knaive.naif.material;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Naif on 3/21/2015.
+ */
+public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    private Context context;
+    List<Information> data = Collections.emptyList();
+    public ViewAdapter(Context context, List<Information> data) {
+        inflater = LayoutInflater.from(context);
+        this.data=data;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_row, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Information current = data.get(position);
+        holder.title.setText(current.title);
+        holder.icon.setImageResource(current.iconId);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title;
+        ImageView icon;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.listText);
+            icon = (ImageView) itemView.findViewById(R.id.listIcon);
+            icon.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context,"Item Clicked at"+getPosition(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
